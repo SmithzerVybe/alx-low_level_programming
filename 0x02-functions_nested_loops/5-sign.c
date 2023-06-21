c3556915e0d3d9cebc423b9358f41160d0237b2e
SmithzerVybe <itzsmithzervybe@gmail.com>
2023-06-21 11:54:15 -0700

    nested loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,24 @@
 #include "main.h"
-/**
-* print_sign -  prints the sign of a number.
-*@i: check character i
-* Return: 1 for postive num, -1 for negative num or zero for anything else
-*/
-int print_sign(int i)
-{
-         if (i > 0)
-         {
-         _putchar(43);
-         return (1);
-         }
-         else if (i < 0)
-         {
-         _putchar(45);
-         return (-1);
-         }
-         else
-         {
-         _putchar(48);
-         return (0);
-         }
-}
+  2 /**
+  3 * print_sign -  prints the sign of a number.
+  4 *@i: check character i
+  5 * Return: 1 for postive num, -1 for negative num or zero for anything else
+  6 */
+  7 int print_sign(int i)
+  8 {
+  9         if (i > 0)
+ 10         {
+ 11         _putchar(43);
+ 12         return (1);
+ 13         }
+ 14         else if (i < 0)
+ 15         {
+ 16         _putchar(45);
+ 17         return (-1);
+ 18         }
+ 19         else
+ 20         {
+ 21         _putchar(48);
+ 22         return (0);
+ 23         }
+ 24 }
